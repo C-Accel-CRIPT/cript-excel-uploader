892a7ca7b84b72e46bd457fef8c5568e02253323
Yisheng Gong <65036338+gongyisheng@users.noreply.github.com>
2022-04-19 13:57:37 -0500

    update transformers

--- transformers.py
+++ transformers.py
@@ -36,6 +36,255 @@ def transform_experiment(group_obj, collection_obj, parsed_experiments, public_f
     return experiment_objs
 
 
+def transform_data(group_obj, experiment_objs, parsed_datas, public_flag):
+    """
+    upload data to the database and return a dict of name:data_url pair
+
+    :param group_obj:
+    :param experiment_objs: (name) : (object of experiment) pair
+    :type experiment_objs: dict
+    :param parsed_datas: parsed data of data_sheet.parsed (data_sheet.parsed)
+    :type parsed_datas: dict
+    :param public_flag: a boolean flag allow users to set whether the data to go public/private
+    :type public_flag: bool
+    :return: (name) : (object of data) pair
+    :rtype: dict
+    """
+    data_objs = {}
+    for data_std_name in parsed_datas:
+        parsed_data = parsed_datas[data_std_name]
+        # Grab Experiment
+        experiment_std_name = parsed_data["experiment"].replace(" ", "").lower()
+        experiment_obj = experiment_objs[experiment_std_name]
+
+        # Create Data
+        data_obj = C.Data(
+            group=group_obj,
+            experiment=experiment_obj,
+            public=public_flag,
+            **parsed_data["base"],
+        )
+        data_objs[data_std_name] = data_obj
+
+    return data_objs
+
+
+def transform_file(group_obj, data_objs, parsed_file, public_flag):
+    """
+    upload data to the database and return a dict of name:file_url pair
+
+    :param api: api connection object
+    :type api: class:`cript.API`
+    :param group_obj: object of group
+    :type group_obj: `cript.Group`
+    :param data_objs: (name) : (obj of data) pair
+    :type data_objs: dict
+    :param parsed_file: parsed data of file_sheet.parsed
+    :type parsed_file: dict
+    :param public_flag: a boolean flag allow users to set whether the data to go public/private
+    :type public_flag: bool
+    :return: (name) : (obj of file) pair
+    :rtype: dict
+    """
+    file_objs = {}
+    for data_std_name in parsed_file:
+        file_dict = parsed_file[data_std_name]
+        # Grab Data
+        data_obj = data_objs[data_std_name]
+        for file in file_dict:
+            # Create File
+            file_obj = C.File(
+                group=group_obj,
+                data=data_obj,
+                public=public_flag,
+                **file["base"],
+            )
+
+            # Update file_urls
+            file_objs[file_obj.checksum] = file_obj
+
+    return file_objs
+
+
+def transform_material(group_obj, data_objs, parsed_material, public_flag):
+    """
+    upload material to the database and return a dict of name:material_url pair
+
+    :param group_obj: obj of group
+    :type group_obj: `cript.nodes.Group`
+    :param data_objs: (name) : (object of data) pair
+    :type data_objs: dict
+    :param parsed_material: reagent_sheet.parsed or product_sheet.parsed
+    :type parsed_material: dict
+    :param public_flag: a boolean flag allow users to set whether the data to go public/private
+    :type public_flag: bool
+    :return: (name) : (url of material) pair
+    :rtype: dict
+    """
+    material_objs = {}
+    for material_std_name in parsed_material:
+
+        material_dict = parsed_material[material_std_name]
+
+        # Create Material object
+        material_obj = C.Material(
+            group=group_obj,
+            components=None,
+            public=public_flag,
+            **material_dict["base"],
+        )
+
+        # Add Prop objects
+        parsed_props = material_dict["prop"]
+        if len(parsed_props) > 0:
+            material_obj.properties = _transform_prop_list(parsed_props, data_objs)
+
+        # Add Identifiers
+        parsed_idens = material_dict["iden"]
+        for key, value in parsed_idens.items():
+            identifier = C.Identifier(key=key, value=value)
+            material_obj.add_identifier(identifier)
+
+        # Add saved Material object to materials dict
+        material_objs[material_std_name] = material_obj
+
+    return material_objs
+
+
+def transform_components(material_objs, parsed_components):
+    for material_std_name in parsed_components:
+        material_obj = material_objs.get(material_std_name)
+        uid = 1
+        for parsed_component in parsed_components[material_std_name]:
+            component_std_name = parsed_component["component"]
+            component_obj = C.Component(
+                component=material_objs.get(component_std_name),
+                component_uid=uid,
+            )
+            material_obj.add_component(component_obj)
+            uid = uid + 1
+
+
+def transform_process(group_obj, experiment_objs, parsed_processes, public_flag):
+    """
+    upload process to the database and return a dict of name:process_url pair
+
+    :param api: api connection object
+    :type api: class:`cript.API`
+    :param group_obj: obj of group
+    :type group_obj: `cript.nodes.Group`
+    :param experiment_objs: (name) : (experiment object) pair
+    :type experiment_objs: dict
+    :param parsed_processes:
+    :type dict
+    :param public_flag: a boolean flag allow users to set whether the data to go public/private
+    :type public_flag: bool
+    :return: (name) : (url of process) pair
+    :rtype: dict
+    """
+    process_objs = {}
+    for experiment_std_name in parsed_processes:
+        # Grab Experiment
+        experiment_obj = experiment_objs.get(experiment_std_name)
+        process_list = parsed_processes[experiment_std_name]
+        prev_process_std_name = None
+        for i in range(len(process_list)):
+            parsed_process = process_list[i]
+            process_name = parsed_process["name"]
+            process_std_name = process_name.replace(" ", "").lower()
+
+            # Create Process
+            process_obj = C.Process(
+                group=group_obj,
+                experiment=experiment_obj,
+                public=public_flag,
+                **parsed_process["base"],
+            )
+
+            if i > 0:
+                prev_process_obj = process_objs.get(prev_process_std_name)
+                process_obj.add_dependent_process(prev_process_obj)
+
+            prev_process_std_name = process_std_name
+            process_objs[process_std_name] = process_obj
+
+    return process_objs
+
+
+def transform_dependent_process(api, process_objs, parsed_dependent_processes):
+    for process_std_name in parsed_dependent_processes:
+        process_obj = process_objs.get(process_std_name)
+        dependent_process_list = parsed_dependent_processes.get(process_std_name)
+        for i in range(len(dependent_process_list)):
+            dependent_process_std_name = dependent_process_list[i]["dependent_process"]
+            dependent_process_obj = process_objs.get(dependent_process_std_name)
+            process_obj.add_dependent_process(dependent_process_obj)
+        api.save(process_obj)
+
+
+def transform_process_ingredient(
+    process_objs, material_objs, parsed_processIngredients
+):
+    """
+    upload step to the database and return a dict of name:step_url pair
+
+    :param process_objs: (name) : (process object) pair
+    :type process_objs: dict
+    :param material_objs: (name) : (object of material) pair
+    :type material_objs: dict
+    :param parsed_processIngredients:
+    :type dict
+    :return: (name) : (step object) pair
+    :rtype: dict
+    """
+    for process_std_name in parsed_processIngredients:
+        # Grab Process
+        process_obj = process_objs[process_std_name]
+
+        if process_obj is None:
+            continue
+
+        for ingredient in parsed_processIngredients[process_std_name]:
+            # Grab Material
+            material_std_name = ingredient["material"]
+            material_obj = material_objs.get(material_std_name)
+            if material_obj is None:
+                continue
+
+            ingredient_obj = C.Ingredient(
+                ingredient=material_obj,
+                quantities=_transform_quantity_list(ingredient["quantity"]),
+                **ingredient["base"],
+            )
+
+            process_obj.add_ingredient(ingredient_obj)
+
+
+def transform_process_product(process_objs, material_objs, parsed_processProducts):
+    """
+    upload step to the database and return a dict of name:step_url pair
+
+    :param process_objs: (name) : (process object) pair
+    :type process_objs: dict
+    :param material_objs: (name) : (object of material) pair
+    :type material_objs: dict
+    :param parsed_processProducts:
+    :type dict
+    :return: (name) : (step object) pair
+    :rtype: dict
+    """
+    for process_std_name in parsed_processProducts:
+        # Grab Process
+        process_obj = process_objs.get(process_std_name)
+        product_list = parsed_processProducts[process_std_name]
+        if process_obj is None:
+            continue
+        for parsed_product in product_list:
+            product_std_name = parsed_product["product"]
+            material_obj = material_objs.get(product_std_name)
+            process_obj.add_product(material_obj)
+
+
 def _transform_cond_list(parsed_conds, data_objs=None):
     """
     Create a list of Cond objects.
@@ -118,269 +367,3 @@ def _replace_field(parsed_object, raw_key, replace_key):
     if raw_key in parsed_object:
         parsed_object[replace_key] = parsed_object[raw_key]
         parsed_object.pop(raw_key)
-
-
-def transform_data(group_obj, experiment_objs, parsed_datas, public_flag):
-    """
-    upload data to the database and return a dict of name:data_url pair
-
-    :param experiment_objs: (name) : (object of experiment) pair
-    :type experiment_objs: dict
-    :param parsed_datas: parsed data of data_sheet.parsed (data_sheet.parsed)
-    :type parsed_datas: dict
-    :param public_flag: a boolean flag allow users to set whether the data to go public/private
-    :type public_flag: bool
-    :return: (name) : (object of data) pair
-    :rtype: dict
-    """
-    data_objs = {}
-    for data_std_name in parsed_datas:
-        parsed_data = parsed_datas[data_std_name]
-        # Grab Experiment
-        experiment_std_name = parsed_data["experiment"].replace(" ", "").lower()
-        experiment_obj = experiment_objs[experiment_std_name]
-
-        # Create Data
-        data_obj = C.Data(
-            group=group_obj,
-            experiment=experiment_obj,
-            public=public_flag,
-            **parsed_data["base"],
-        )
-        data_objs[data_std_name] = data_obj
-
-    return data_objs
-
-
-def transform_file(group_obj, data_objs, parsed_file, public_flag):
-    """
-    upload data to the database and return a dict of name:file_url pair
-
-    :param api: api connection object
-    :type api: class:`cript.API`
-    :param group_obj: object of group
-    :type group_obj: `cript.Group`
-    :param data_objs: (name) : (obj of data) pair
-    :type data_objs: dict
-    :param parsed_file: parsed data of file_sheet.parsed
-    :type parsed_file: dict
-    :param public_flag: a boolean flag allow users to set whether the data to go public/private
-    :type public_flag: bool
-    :return: (name) : (obj of file) pair
-    :rtype: dict
-    """
-    file_objs = {}
-    for key in parsed_file:
-        file_dict = parsed_file[key]
-        # Grab Data
-        data_obj = data_objs[key]
-        for file in file_dict:
-            # Replace field name
-            _replace_field(file["base"], "path", "source")
-
-            # Create File
-            file_obj = C.File(
-                group=group_obj,
-                data=data_obj,
-                public=public_flag,
-                **file["base"],
-            )
-
-            # Update file_urls
-            if key not in file_objs:
-                file_objs[key] = []
-            file_objs[key].append(file_obj)
-
-    return file_objs
-
-
-def transform_material(group_obj, data_objs, parsed_material, public_flag):
-    """
-    upload material to the database and return a dict of name:material_url pair
-
-    :param group_obj: obj of group
-    :type group_obj: `cript.nodes.Group`
-    :param data_objs: (name) : (object of data) pair
-    :type data_objs: dict
-    :param parsed_material: reagent_sheet.parsed or product_sheet.parsed
-    :type parsed_material: dict
-    :param public_flag: a boolean flag allow users to set whether the data to go public/private
-    :type public_flag: bool
-    :return: (name) : (url of material) pair
-    :rtype: dict
-    """
-    material_objs = {}
-    for material_std_name in parsed_material:
-
-        material_dict = parsed_material[material_std_name]
-
-        # Create Material object
-        material_obj = C.Material(
-            group=group_obj,
-            components=None,
-            public=public_flag,
-            **material_dict["base"],
-        )
-
-        # Add Prop objects
-        parsed_props = material_dict["prop"]
-        if len(parsed_props) > 0:
-            material_obj.properties = _transform_prop_list(parsed_props, data_objs)
-
-        # Add Identifiers
-        parsed_idens = material_dict["iden"]
-        for key, value in parsed_idens.items():
-            identifier = C.Identifier(key=key, value=value)
-            material_obj.add_identifier(identifier)
-
-        # Add saved Material object to materials dict
-        material_objs[material_std_name] = material_obj
-
-    return material_objs
-
-
-def transform_components(api, material_objs, parsed_components):
-    for material_std_name in parsed_components:
-        material_obj = material_objs.get(material_std_name)
-        uid = 1
-        for parsed_component in parsed_components[material_std_name]:
-            component_std_name = parsed_component["component"]
-            component_obj = C.Component(
-                component=material_objs.get(component_std_name),
-                component_uid=uid,
-            )
-            material_obj.add_component(component_obj)
-            uid = uid + 1
-        api.save(material_obj)
-
-
-def transform_process(api, group_obj, experiment_objs, parsed_processes, public_flag):
-    """
-    upload process to the database and return a dict of name:process_url pair
-
-    :param api: api connection object
-    :type api: class:`cript.API`
-    :param group_obj: obj of group
-    :type group_obj: `cript.nodes.Group`
-    :param experiment_objs: (name) : (experiment object) pair
-    :type experiment_objs: dict
-    :param parsed_processes:
-    :type dict
-    :param public_flag: a boolean flag allow users to set whether the data to go public/private
-    :type public_flag: bool
-    :return: (name) : (url of process) pair
-    :rtype: dict
-    """
-    process_objs = {}
-    for experiment_std_name in parsed_processes:
-        # Grab Experiment
-        experiment_obj = experiment_objs.get(experiment_std_name)
-        process_list = parsed_processes[experiment_std_name]
-        prev_process_std_name = None
-        for i in range(len(process_list)):
-            parsed_process = process_list[i]
-            process_name = parsed_process["name"]
-            process_std_name = process_name.replace(" ", "").lower()
-
-            process_search_result = api.search(
-                C.Process,
-                {
-                    "group": group_obj.uid,
-                    "experiment": experiment_obj.uid,
-                    "name": process_name,
-                },
-            )
-            if process_search_result["count"] > 0:
-                url = process_search_result["results"][0]["url"]
-                process_obj = api.get(url)
-                print(f"Process node [{process_obj.name}] already exists")
-            else:
-                # Create Process
-                process_obj = C.Process(
-                    group=group_obj,
-                    experiment=experiment_obj,
-                    public=public_flag,
-                    **parsed_process["base"],
-                )
-                prev_process_std_name = process_std_name
-                if i > 0:
-                    prev_process_obj = process_objs.get(prev_process_std_name)
-                    process_obj.add_dependent_process(prev_process_obj)
-
-            process_objs[process_std_name] = process_obj
-
-    return process_objs
-
-
-def transform_dependent_process(api, process_objs, parsed_dependent_processes):
-    for process_std_name in parsed_dependent_processes:
-        process_obj = process_objs.get(process_std_name)
-        dependent_process_list = parsed_dependent_processes.get(process_std_name)
-        for i in range(len(dependent_process_list)):
-            dependent_process_std_name = dependent_process_list[i]["dependent_process"]
-            dependent_process_obj = process_objs.get(dependent_process_std_name)
-            process_obj.add_dependent_process(dependent_process_obj)
-        api.save(process_obj)
-
-
-def transform_process_ingredient(
-    process_objs, material_objs, parsed_processIngredients
-):
-    """
-    upload step to the database and return a dict of name:step_url pair
-
-    :param process_objs: (name) : (process object) pair
-    :type process_objs: dict
-    :param material_objs: (name) : (object of material) pair
-    :type material_objs: dict
-    :param parsed_processIngredients:
-    :type dict
-    :return: (name) : (step object) pair
-    :rtype: dict
-    """
-    for process_std_name in parsed_processIngredients:
-        # Grab Process
-        process_obj = process_objs[process_std_name]
-
-        if process_obj is None:
-            continue
-
-        for ingredient in parsed_processIngredients[process_std_name]:
-            # Grab Material
-            material_std_name = ingredient["material"]
-            material_obj = material_objs.get(material_std_name)
-            if material_obj is None:
-                continue
-
-            ingredient_obj = C.Ingredient(
-                ingredient=material_obj,
-                quantities=_transform_quantity_list(ingredient["quantity"]),
-                **ingredient["base"],
-            )
-
-            process_obj.add_ingredient(ingredient_obj)
-
-
-def transform_process_product(process_objs, material_objs, parsed_processProducts):
-    """
-    upload step to the database and return a dict of name:step_url pair
-
-    :param process_objs: (name) : (process object) pair
-    :type process_objs: dict
-    :param material_objs: (name) : (object of material) pair
-    :type material_objs: dict
-    :param parsed_processProducts:
-    :type dict
-    :return: (name) : (step object) pair
-    :rtype: dict
-    """
-    for process_std_name in parsed_processProducts:
-        # Grab Process
-        process_obj = process_objs.get(process_std_name)
-        product_list = parsed_processProducts[process_std_name]
-        if process_obj is None:
-            continue
-        for parsed_product in product_list:
-            product_std_name = parsed_product["product"]
-            material_obj = material_objs.get(product_std_name)
-            process_obj.add_product(material_obj)
