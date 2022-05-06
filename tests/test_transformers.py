677097496a4c416209b355eb267ec99cf417e166
Yisheng Gong <65036338+gongyisheng@users.noreply.github.com>
2022-05-06 01:30:40 -0500

    add test cases for transformer

---
+++ tests/test_transformers.py
@@ -0,0 +1,26 @@
+import pytest
+from src.transformers import (
+    transform_experiment,
+    transform_data,
+    transform_file,
+    transform_material,
+    transform_components,
+    transform_process,
+    transform_prerequisite_process,
+    transform_process_ingredient,
+    transform_process_product,
+)
+from tests.cases.case_transformers import (
+    group_obj,
+    collection_obj,
+    public_flag,
+    case_transform_experiment_true,
+)
+
+
+@pytest.mark.parametrize("parsed_experiments, expected", case_transform_experiment_true)
+def test_transform_experiment(parsed_experiments, expected):
+    actual = transform_experiment(
+        group_obj, collection_obj, parsed_experiments, public_flag
+    )
+    assert all([a == b for a, b in zip(actual, expected)])
