342d0e1429b0fbe13fc07e25b13c283bd0aa4ca6
Navid Hariri <navidhariri25@gmail.com>
2022-09-28 17:39:23 -0700

    added name flag to pyinstaller_compile_windows.sh

--- pyinstaller_compile_windows.sh
+++ pyinstaller_compile_windows.sh
@@ -1,1 +1,1 @@
-python -m eel .\src\python\gui_main.py .\src\web\ --onefile --noconsole --clean --icon=.\src\assets\logo_condensed.ico
+python -m eel .\src\python\gui_main.py .\src\web\ --onefile --noconsole --clean --name excel_uploader_gui_windows --icon=.\src\assets\logo_condensed.ico
