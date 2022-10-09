4d96be8f1bace068b8531eccbf85fcf680dec7d8
Navid Hariri <navidhariri25@gmail.com>
2022-10-08 17:42:05 -0700

    added new code design for single options and nested options and getting preferred units function

--- utils/material_sheet_keys.py
+++ utils/material_sheet_keys.py
@@ -1,43 +1,99 @@
 import pandas as pd
 
 
-class MaterialAutoFill:
+# TODO this needs to be renamed because it is very unintuitive
+def get_all_excel_sheets(source_excel_file):
+    """
+    takes the whole source Excel file, gets the name of all the sheets that exist
+    loops through every sheet and fills up all_sheets_dict with a df of each sheet
+    and returns the dict of all sheets df
+    :params source_excel_file: Excel file with vocabulary sheets
+    :returns all_sheets_dict: dict filled with df of every sheet
+    """
 
-    def __init__(self):
-        self.material_file_path_to_read_from = None
-        self.material_file_path_to_write_to = None
-        self.material_sheet_to_write_to = None
-        self.all_sheets_df_dict = {}
-        self.all_row_2_possible_values = None
+    all_sheets_dict = {}
 
-    def read_excel_file(self, source_excel_file, ):
-        """
-         This method reads each sheet of the Excel file and fills up all_sheets_df_dict
-         for all other methods to grab from and use
-        :params source_excel_file: Excel file with vocabulary sheets
-        :returns: None
-        """
+    # loop through all sheets and add them as a df to all_sheets_dict
+    excel_file = pd.ExcelFile(source_excel_file)
 
-        excel_file = pd.ExcelFile(source_excel_file)
+    for sheet_name in excel_file.sheet_names:
+        df = pd.read_excel(excel_file, sheet_name)
 
-        # put each sheet df into the dict for other functions to use
-        self.all_sheets_df_dict["material_property"] = pd.read_excel(excel_file, "material_property")
-        self.all_sheets_df_dict["citation_type"] = pd.read_excel(excel_file, "citation_type")
-        self.all_sheets_df_dict["condition"] = pd.read_excel(excel_file, "condition")
+        # adding metadata of sheet name into the df to be used later
+        df.sheet_name = sheet_name
 
-    def property_colon_condition(self, property_df, condition_df):
-        pass
+        all_sheets_dict[sheet_name] = df
 
-    def property_colon_citations(self, property_df, citation_df):
-        pass
+    return all_sheets_dict
 
-    def property_colon_attribute(self, property_df, citation_df):
-        pass
 
-    def write_to_dest_excel_sheet(self, df):
-        pass
+def get_preferred_unit(row):
+    """
+    checks if there is a preferred unit
+    if no preferred unit then it uses the SI unit
+    if no preferred unit or SI unit, then it returns an empty string
+
+    :params row: pandas series
+    :returns: a string that has the preferred units or an empty string
+    """
+    preferred_unit = row["Preferred unit"]
+    si_unit = row["SI unit"]
+
+    # has a preferred unit
+    if preferred_unit is not "" and preferred_unit is not "None":
+        return preferred_unit
+
+    # does not have preferred unit, but has SI unit
+    elif (preferred_unit is "" or preferred_unit is "None") and (si_unit is not "" and si_unit is not "None"):
+        return si_unit
+
+    # si unit and preferred unit are both empty or none
+    elif (preferred_unit is "" or preferred_unit is "None") and (si_unit is "" or si_unit is "None"):
+        return ""
+
+
+def single_options(sheet_df):
+    """
+    takes a single Excel sheet df, and gets the Row 1 value, Row 2, value, unit, and description
+    appends them to a df on every loop
+    returns the df to be appended to the full list of all possible options
+
+    :params sheet_df: pandas dataframe
+    :returns df: pandas dataframe
+    """
+    df = pd.DataFrame()
+
+    # TODO I don't think this way of appending to the df is working
+    for row in sheet_df.iterrows():
+        df["Row 1 Value"] = sheet_df.sheet_name
+        df["Row 2 Value"] = row[1]["Name"]
+
+        # be sure you do not have "" or "None" in preferred values
+        # can probably add a check for having 2 preferred values as well
+        # if there is no preferred value see if there is SI Value instead
+        # use get_preferred_unit( ) here instead
+        df["unit"] = row[1]["Preferred unit"]
+        df["description"] = row[1]["Description"]
+
+
+# TODO make this abstract so it can work for all of them
+def property_colon_condition(property_df, condition_df):
+    all_property_condition = pd.DataFrame()
+
+    for property_row in property_df.iterrows():
+        for condition_row in condition_df.iterrows():
+            df = pd.DataFrame()
+            df["Row 1 Value"] = condition_df.sheet_name
+            df["Row 2 Value"] = f"{property_row[1].iloc.name}:{condition_row[1].iloc.name}"
+
+
+def write_to_dest_excel_sheet(df):
+    pass
 
 
 if __name__ == "__main__":
-    material_options = MaterialAutoFill()
-    material_options.read_excel_file("./excel_files/source.xlsx")
+    all_sheets_df = get_all_excel_sheets("./excel_files/source.xlsx")
+    full_options_df = pd.DataFrame()
+
+    # working on getting the single version working first
+    single_options(all_sheets_df["property"])
