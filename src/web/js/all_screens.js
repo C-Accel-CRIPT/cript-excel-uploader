966da0332b1e911d37b5338f7ebc053383885976
Navid Hariri <navidhariri25@gmail.com>
2022-11-02 19:09:51 -0700

    fixed start_screen having broken feedback
    all_screens.js: put restartStartScreen.js into a function that can be used by restartAllScreens function and start_screen.js submitForm() function
    
    start_screen.js: clearing the start_screen form validations before sending user input to not get any leftover/wrong feedback after submission
    
    start_screen.js: using getInputElements() function instead of manually getting each element

--- src/web/js/all_screens.js
+++ src/web/js/all_screens.js
@@ -40,7 +40,34 @@ function uploadAgain() {
     restarts the start_screen, loading_screen, globus_screen, and error_screen
 */
 function restartAllScreens() {
-//    restart start screen
+
+// restart start screen
+    restartStartScreen();
+
+// restart loading screen
+    updateLoadingBar(0);
+    // removing updating label from what the last thing it was
+    document.getElementById("uploading-specifics").textContent = "...";
+
+// restart globus screen
+    // clears out the form of the token that they have already inputted and ready to accept input
+    // the link for globus will get auto generated anyways
+    document.getElementById("globus-auth-token-input").value = "";
+
+// restart error screen
+    // remove any errors that are already appended to the error window
+document.getElementById("error-window").textContent = "";
+
+}
+
+/*
+function used when start screen needs to be restarted to used again
+it enables upload button, changes the text to upload, and
+removes any feedback of valid or invalid from the screen.
+However, it leaves all the original values that were inputted into the fields
+*/
+function restartStartScreen() {
+ //    restart start screen
     // enable the start screen button
     let uploadButton = document.getElementById("upload-button");
     uploadButton.textContent = "Upload";
@@ -71,20 +98,4 @@ function restartAllScreens() {
     // remove feedback from excelFile
     inputElements.excelFile.classList.remove("is-valid");
     inputElements.excelFile.classList.remove("is-invalid");
-
-
-// restart loading screen
-    updateLoadingBar(0);
-    // removing updating label from what the last thing it was
-    document.getElementById("uploading-specifics").textContent = "...";
-
-// restart globus screen
-    // clears out the form of the token that they have already inputted and ready to accept input
-    // the link for globus will get auto generated anyways
-    document.getElementById("globus-auth-token-input").value = "";
-
-// restart error screen
-    // remove any errors that are already appended to the error window
-document.getElementById("error-window").textContent = "";
-
 }
